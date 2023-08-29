dd525255f51b86e7ac69f4677ea9f1cf56bf0931
André Staltz <andre@staltz.com>
2023-08-29 20:42:37 +0300

    new useTcp option on RelayerOptions (#6)

--- src/models/export-models.ts
+++ src/models/export-models.ts
@@ -7,6 +7,7 @@ export type RelayerOptions = {
   pubSubTopic?: string;
   additionalDirectPeers?: string[];
   peerDiscoveryTimeout?: number;
+  useTcp?: boolean;
 };
 
 export type RelayerConnectionStatusCallback = (
