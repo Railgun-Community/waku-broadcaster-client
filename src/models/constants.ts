69900313867a3cbe033f9d904852cd1e3dbae0cc
zy0n <zy0n.barto@gmail.com>
2023-11-20 12:23:41 -0500

    Update the default peers for waku.

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -2,5 +2,6 @@ export const WAKU_RAILGUN_PUB_SUB_TOPIC = '/waku/2/railgun-relayer';
 
 export const WAKU_RAILGUN_DEFAULT_PEERS: string[] = [
   '/dns4/relayerv4.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAmCMBVq9am26T61B7FyZ6JbEDusH4c7M7AYVMwNnRuP2cg',
-  '/dns4/rel.cribbagerelayer.xyz/tcp/8000/wss/p2p/16Uiu2HAkv4fxpy3ScCogjPcw1voropnmy37UJxRdAC2DfW5F7ssZ',
+  '/dns4/fleet.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAkyCqm8368Z1Y1pvrBcQQtb66Mjj7uvyAKGdwur3uCbqfb',
+  '/dns4/fleet.wecamefromapes.com/tcp/8001/wss/p2p/16Uiu2HAmTCCZFbQxDNF1TL9TKSPXtfF9QJtNWhCFE5M8UNzk7DQv',
 ];
