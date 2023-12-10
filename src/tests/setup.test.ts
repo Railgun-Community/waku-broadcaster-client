d22ac158f260b55ea977d92a5414481d1b5a5ecb
Pete S <ptaylorsimpson@gmail.com>
2023-12-10 10:27:12 -0800

    Updates deps

--- src/tests/setup.test.ts
+++ src/tests/setup.test.ts
@@ -43,6 +43,6 @@ export const initTestEngine = (useNativeArtifacts = false) => {
     testArtifactStore,
     useNativeArtifacts,
     false, // skipMerkletreeScans
-    'mock-poi-url', // poiNodeURL
+    ['mock-poi-url'], // poiNodeURLs
   );
 };
