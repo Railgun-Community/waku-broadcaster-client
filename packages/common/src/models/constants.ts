0fc11797da54a690daea50426c424e4c5ac80341
zy0n <zy0n.barto@gmail.com>
2024-05-17 15:01:31 -0400

    update fleet

--- packages/common/src/models/constants.ts
+++ packages/common/src/models/constants.ts
@@ -2,12 +2,10 @@ export const WAKU_RAILGUN_PUB_SUB_TOPIC = '/waku/2/railgun-broadcaster';
 
 export const WAKU_RAILGUN_DEFAULT_PEERS_WEB: string[] = [
   // Some Websocket broadcasters (web friendly):
-  '/dns4/teelf.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAkygexqAtchrhYVqLFBTXFEEZPtz8C8ZnSP6iYy1s22mfW',
-  '/dns4/fleet.wecamefromapes.com/tcp/8000/wss/p2p/16Uiu2HAkyCqm8368Z1Y1pvrBcQQtb66Mjj7uvyAKGdwur3uCbqfb',
+  '/dns4/horsey.horsewithsixlegs.xyz/tcp/8000/wss/p2p/16Uiu2HAmLWDFk5nGXgLx2Mz2deiEMRya1HNcvWELMDodkAC891Sd',
 ];
 
 export const WAKU_RAILGUN_DEFAULT_PEERS_NODE: string[] = [
   // Some TCP broadcasters (node friendly):
-  '/dns4/teelf.wecamefromapes.com/tcp/60000/p2p/16Uiu2HAkygexqAtchrhYVqLFBTXFEEZPtz8C8ZnSP6iYy1s22mfW',
-  '/dns4/fleet.wecamefromapes.com/tcp/60000/p2p/16Uiu2HAkyCqm8368Z1Y1pvrBcQQtb66Mjj7uvyAKGdwur3uCbqfb',
+  '/dns4/horsey.horsewithsixlegs.xyz/tcp/60000/p2p/16Uiu2HAmLWDFk5nGXgLx2Mz2deiEMRya1HNcvWELMDodkAC891Sd',
 ];
